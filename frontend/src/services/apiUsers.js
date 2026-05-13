dc4fb8d8d793d0b43411c6f016bc0da7876bd75d
stexxer11 <rolandorar19@gmail.com>
2026-05-13 11:47:26 -0500

    USUARIO CASI LISTO

---
+++ frontend/src/services/apiUsers.js
@@ -0,0 +1,19 @@
+import { supabase } from "../supabaseClient"
+
+export const registerUserApi = async (deviceId, name) => {
+  const { data, error } = await supabase
+    .from("users")
+    .upsert([
+      {
+        device_id: deviceId,
+        name: name,
+      },
+    ])
+
+  if (error) {
+    console.error("Error registrando usuario:", error)
+    return null
+  }
+
+  return data
+}
